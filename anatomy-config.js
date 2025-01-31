a7e1c1a7c6ca11131282634b886b99b864c5d37a
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-01-31 15:45:28 -0500

    Update anatomy-config.js

--- anatomy-config.js
+++ anatomy-config.js
@@ -2,47 +2,47 @@ var basic_config = {
     "basic_1": {//head
         "hover": "HEAD",//info of the popup
         "url": "https://form.jotform.com/250295484302051?selectedPlacement=HEAD",//link to JotForm with pre-filled selection
-        "target": "_blank",// opens in a new tab
+        "target": "_self",// use "_blank", "_self", or "none"
         "active": true//true/false to show/hide
     },
     "basic_2": {//neck
         "hover": "NECK",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=NECK", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=NECK", "target": "_self",
         "active": true
     },
     "basic_3": {//chest
         "hover": "CHEST",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=CHEST", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=CHEST", "target": "_self",
         "active": true
     },
     "basic_4": {//abdomen
         "hover": "ABDOMEN",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ABDOMEN", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ABDOMEN", "target": "_self",
         "active": true
     },
     "basic_5": {//pelvis
         "hover": "PELVIS",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=PELVIS", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=PELVIS", "target": "_self",
         "active": true
     },
     "basic_6": {//arm-rt
         "hover": "ARM [RT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ARM%20[RT]", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ARM%20[RT]", "target": "_self",
         "active": true
     },
     "basic_7": {//arm-lt
         "hover": "ARM [LT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ARM%20[LT]", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ARM%20[LT]", "target": "_self",
         "active": true
     },
     "basic_8": {//leg-rt
         "hover": "LEG [RT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=LEG%20[RT]", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=LEG%20[RT]", "target": "_self",
         "active": true
     },
     "basic_9": {//leg-lt
         "hover": "LEG [LT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=LEG%20[LT]", "target": "_blank",
+        "url": "https://form.jotform.com/250295484302051?selectedPlacement=LEG%20[LT]", "target": "_self",
         "active": true
     }
 };
