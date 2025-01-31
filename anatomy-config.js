37584c44408b01fdfbb0601616a86eba2601666e
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-01-30 20:49:19 -0500

    Update anatomy-config.js

--- anatomy-config.js
+++ anatomy-config.js
@@ -1,6 +1,6 @@
 var basic_config = {
     "basic_1":{//head
-        "hover": "HEAD",//info of the popup
+        "hover": "Head\n$$$\n🟢🟡🔴",//info of the popup
         "url": "https://www.humananatomyillustrations.com/",//link to any webpage
         "target": "none",// use "_blank", "_self", or "none"
         "active": true//true/false to show/hide
