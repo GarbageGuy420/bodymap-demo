c99cda6fab4f78a6042cfa566d5344224c3657ab
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-01-31 17:12:08 -0500

    Update anatomy-config.js

--- anatomy-config.js
+++ anatomy-config.js
@@ -1,48 +1,60 @@
+function sendSelection(bodyPart) {
+    window.parent.postMessage({ selectedPlacement: bodyPart }, "*");
+}
+
 var basic_config = {
     "basic_1": {//head
-        "hover": "HEAD",//info of the popup
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=HEAD",//link to JotForm with pre-filled selection
-        "target": "_self",// use "_blank", "_self", or "none"
-        "active": true//true/false to show/hide
+        "hover": "HEAD",
+        "url": "javascript:sendSelection('HEAD')",
+        "target": "none",
+        "active": true
     },
     "basic_2": {//neck
         "hover": "NECK",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=NECK", "target": "_self",
+        "url": "javascript:sendSelection('NECK')",
+        "target": "none",
         "active": true
     },
     "basic_3": {//chest
         "hover": "CHEST",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=CHEST", "target": "_self",
+        "url": "javascript:sendSelection('CHEST')",
+        "target": "none",
         "active": true
     },
     "basic_4": {//abdomen
         "hover": "ABDOMEN",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ABDOMEN", "target": "_self",
+        "url": "javascript:sendSelection('ABDOMEN')",
+        "target": "none",
         "active": true
     },
     "basic_5": {//pelvis
         "hover": "PELVIS",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=PELVIS", "target": "_self",
+        "url": "javascript:sendSelection('PELVIS')",
+        "target": "none",
         "active": true
     },
     "basic_6": {//arm-rt
         "hover": "ARM [RT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ARM%20[RT]", "target": "_self",
+        "url": "javascript:sendSelection('ARM [RT]')",
+        "target": "none",
         "active": true
     },
     "basic_7": {//arm-lt
         "hover": "ARM [LT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=ARM%20[LT]", "target": "_self",
+        "url": "javascript:sendSelection('ARM [LT]')",
+        "target": "none",
         "active": true
     },
     "basic_8": {//leg-rt
         "hover": "LEG [RT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=LEG%20[RT]", "target": "_self",
+        "url": "javascript:sendSelection('LEG [RT]')",
+        "target": "none",
         "active": true
     },
     "basic_9": {//leg-lt
         "hover": "LEG [LT]",
-        "url": "https://form.jotform.com/250295484302051?selectedPlacement=LEG%20[LT]", "target": "_self",
+        "url": "javascript:sendSelection('LEG [LT]')",
+        "target": "none",
         "active": true
     }
 };
