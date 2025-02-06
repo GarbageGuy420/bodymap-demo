5bc23821a84e948b4f42f9ec05315a626399ddfb
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-02-06 16:13:10 -0500

    Update anatomy-script.js

--- anatomy-script.js
+++ anatomy-script.js
@@ -2,84 +2,100 @@
 
     "use strict";
 
-    // Load Jotform Widget API
-    (function() {
-        var po = document.createElement('script');
-        po.type = 'text/javascript';
-        po.async = true;
-        po.src = '//js.jotform.com/JotFormCustomWidget.min.js?onload=onLoadCallback';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(po, s);
-    })();
-
     function isTouchEnabled() {
-        return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+    return (('ontouchstart' in window)
+        || (navigator.MaxTouchPoints > 0)
+        || (navigator.msMaxTouchPoints > 0));
     }
 
     $(document).ready(function () {
-        $("path[id^='basic_']").each(function (i, e) {
+        $("path[id^=\"basic_\"]").each(function (i, e) {
             addEvent($(e).attr('id'));
         });
     });
 
-    function addEvent(id) {
+    function addEvent(id, relationId) {
         var _obj = $('#' + id);
         $('#basic-wrapper').css({'opacity': '1'});
 
         _obj.attr({'fill': 'rgba(255, 0, 0, 0)', 'stroke': 'rgba(255, 102, 102, 1)'});
-        _obj.attr({'cursor': 'pointer'});
+        _obj.attr({'cursor': 'default'});
 
         if (basic_config[id]['active'] === true) {
             if (isTouchEnabled()) {
+                var touchmoved;
                 _obj.on('touchend', function (e) {
-                    handleSelection(id, e);
-                });
-            } else {
-                _obj.on('click', function (e) {
-                    handleSelection(id, e);
+                    if (touchmoved !== true) {
+                        _obj.on('touchstart', function (e) {
+                            let touch = e.originalEvent.touches[0];
+                            let x = touch.pageX - 10, y = touch.pageY + (-15);
+
+                            let $basicatip = $('#tip-basic');
+                            let basicanatomytipw = $basicatip.outerWidth(),
+                                basicanatomytiph = $basicatip.outerHeight();
+
+                            x = (x + basicanatomytipw > $(document).scrollLeft() + $(window).width()) ? x - basicanatomytipw - (20 * 2) : x
+                            y = (y + basicanatomytiph > $(document).scrollTop() + $(window).height()) ? $(document).scrollTop() + $(window).height() - basicanatomytiph - 10 : y
+
+                            if (basic_config[id]['target'] !== 'none') {
+                                _obj.css({'fill': 'rgba(255, 0, 0, 0.7)'});
+                            }
+                            $basicatip.show().html(basic_config[id]['hover']);
+                            $basicatip.css({left: x, top: y})
+                        })
+                        _obj.on('touchend', function () {
+                            _obj.css({'fill': 'rgba(255, 0, 0, 0)'});
+                            if (basic_config[id]['target'] === '_blank') {
+                                window.open(basic_config[id]['url']);
+                            } else if (basic_config[id]['target'] === '_self') {
+                                window.parent.location.href = basic_config[id]['url'];
+                            }
+                            $('#tip-basic').hide();
+                        })
+                    }
+                }).on('touchmove', function (e) {
+                    touchmoved = true;
+                }).on('touchstart', function () {
+                    touchmoved = false;
                 });
             }
-        }
-    }
+            _obj.attr({'cursor': 'pointer'});
 
-    function handleSelection(id, event) {
-        var selectedRegion = basic_config[id]['hover'];
-        console.log("Selected region:", selectedRegion);
+            _obj.on('mouseenter', function () {
+                $('#tip-basic').show().html(basic_config[id]['hover']);
+                _obj.css({'fill': 'rgba(255, 0, 0, 0.3)'})
+            }).on('mouseleave', function () {
+                $('#tip-basic').hide();
+                _obj.css({'fill': 'rgba(255, 0, 0, 0)'});
+            })
+            if (basic_config[id]['target'] !== 'none') {
+                _obj.on('mousedown', function () {
+                    _obj.css({'fill': 'rgba(255, 0, 0, 0.7)'});
+                })
+            }
+            _obj.on('mouseup', function () {
+                _obj.css({'fill': 'rgba(255, 0, 0, 0.3)'});
+                if (basic_config[id]['target'] === '_blank') {
+                    window.open(basic_config[id]['url']);
+                } else if (basic_config[id]['target'] === '_self') {
+                    window.parent.location.href = basic_config[id]['url'];
+                }
+            })
+            _obj.on('mousemove', function (e) {
+                let x = e.pageX + 10, y = e.pageY + 15;
 
-        // Highlight the selected area
-        $('[id^="basic_"]').css({'fill': 'rgba(255, 0, 0, 0)'}); // Reset others
-        $('#' + id).css({'fill': 'rgba(255, 0, 0, 0.7)'});
+                let $abasic = $('#tip-basic');
+                let basicanatomytipw = $abasic.outerWidth(), basicanatomytiph = $abasic.outerHeight();
 
-        // Send selected body region data to Jotform
-        sendSelectionToJotform(selectedRegion);
-    }
+                x = (x + basicanatomytipw > $(document).scrollLeft() +
+                    $(window).width()) ? x - basicanatomytipw - (20 * 2) : x
+                y = (y + basicanatomytiph > $(document).scrollTop() + $(window).height()) ?
+                    $(document).scrollTop() + $(window).height() - basicanatomytiph - 10 : y
 
-    // Function to send the selected body region to Jotform
-    function sendSelectionToJotform(region) {
-        if (typeof JFCustomWidget !== 'undefined' && JFCustomWidget.sendData) {
-            JFCustomWidget.sendData({
-                valid: true,
-                value: region
-            });
+                $abasic.css({left: x, top: y})
+            })
         } else {
-            console.error("JFCustomWidget is not available.");
+            _obj.hide();
         }
     }
-
-    $(document).on("mouseenter", "path[id^='basic_']", function() {
-        $(this).css({'fill': 'rgba(255, 0, 0, 0.5)'});
-    });
-
-    $(document).on("mouseleave", "path[id^='basic_']", function() {
-        if (!$(this).hasClass("selected")) {
-            $(this).css({'fill': 'rgba(255, 0, 0, 0)'});
-        }
-    });
-
-    $(document).on("click", "#reset-selection", function() {
-        $("path[id^='basic_']").removeClass("selected").css({'fill': 'rgba(255, 0, 0, 0)'});
-        JFCustomWidget.sendData({ valid: false, value: "" });
-        console.log("Selection reset");
-    });
-
 })(jQuery);
